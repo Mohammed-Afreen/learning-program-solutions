6012f267d539903d19c803f89191ec81f2efd6f6
Mohammed-Afreen <mohammed.afreen2005@gmail.com>
2025-06-26 19:16:32 +0530

    Create MyService.java

---
+++ Week 2_TDD using JUnit5 and Mockito/1_Mocking and Stubbing/MyService.java
@@ -0,0 +1,13 @@
+package com.example.service;
+import com.example.api.ExternalApi;
+public class MyService {
+    private final ExternalApi externalApi;
+
+    public MyService(ExternalApi externalApi) {
+        this.externalApi = externalApi;
+    }
+
+    public String fetchData() {
+        return externalApi.getData();
+    }
+}
