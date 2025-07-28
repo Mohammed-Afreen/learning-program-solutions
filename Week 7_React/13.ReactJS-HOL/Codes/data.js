a5b7a9447e2205c5936f54a70011eff69253d443
Mohammed-Afreen <mohammed.afreen2005@gmail.com>
2025-07-28 19:10:04 +0530

    Create data.js

---
+++ Week 7_React/13.ReactJS-HOL/Codes/data.js
@@ -0,0 +1,15 @@
+export const books = [
+  { id: 101, bname: 'Master React', price: 670 },
+  { id: 102, bname: 'Deep Dive into Angular 11', price: 800 },
+  { id: 103, bname: 'Mongo Essentials', price: 450 }
+];
+
+export const blogs = [
+  { id: 1, title: 'React Hooks Guide' },
+  { id: 2, title: 'Understanding Virtual DOM' }
+];
+
+export const courses = [
+  { id: 201, cname: 'React Basics', duration: '4 weeks' },
+  { id: 202, cname: 'Fullstack Development', duration: '8 weeks' }
+];
