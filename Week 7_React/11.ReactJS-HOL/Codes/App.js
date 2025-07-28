2aea1281e94b3e3e6218a98aebd78102e1ac8004
Mohammed-Afreen <mohammed.afreen2005@gmail.com>
2025-07-28 18:31:08 +0530

    Create App.js

---
+++ Week 7_React/11.ReactJS-HOL/Codes/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Counter from './Counter';
+import CurrencyConverter from './CurrencyConverter';
+
+function App() {
+  return (
+    <div>
+      <h1>Event Examples App</h1>
+      <Counter />
+      <hr />
+      <CurrencyConverter />
+    </div>
+  );
+}
+
+export default App;
